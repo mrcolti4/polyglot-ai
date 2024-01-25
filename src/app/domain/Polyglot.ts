b988c9f6171501e8680f16fa6494aab6c0917033
mrcolti4 <kbulygin456@gmail.com>
2024-01-25 18:14:39 +0200

    feat(polyglot-audio): add routes controlls for audio uploading and reading

--- src/app/domain/Polyglot.ts
+++ src/app/domain/Polyglot.ts
@@ -8,15 +8,21 @@ import {
   Param,
   Req,
 } from "routing-controllers";
+import { Response, Request } from "express";
 import { v4 as uuid } from "uuid";
+import getRawBody from "raw-body";
 
 import { IAssistantInstructions } from "types/assistant-instructions";
 import { ISendMessage } from "types/send-message";
 import { AuthRequest } from "types/controllerAction";
+
+import { AudioProcessing } from "utils/AudioProcessing";
 import { Assistant } from "utils/Assistant";
 import { db } from "utils/Firebase";
+
 import { ApiResponse } from "helpers/ApiResponse";
 import { ApiError } from "helpers/ApiError";
+
 import { Authenticate } from "app/middlewares/authenticate";
 
 @JsonController("/polyglot")
@@ -100,5 +106,61 @@ export default class Polyglot {
       .update({ messages: admin.firestore.FieldValue.arrayUnion(responseObj) });
 
     return new ApiResponse(true, messages, `Assistant respond!`);
+  }
+
+  @Post("/save-file")
+  async saveFile() {
+    const buffer = await AudioProcessing.fromTextToAudio("Hello world");
+    await AudioProcessing.saveAudioFile(buffer, {
+      convId: "1234-5678",
+      role: "user",
+      messageId: "buffer_id",
+    });
+
+    return new ApiResponse(true, null, `File saved`);
+  }
+
+  @Get("/get-file")
+  async getFile(req: Request, res: Response) {
+    const range = req.headers.range || "";
+    if (range) {
+      res.status(400).send("Range required");
+    }
+    const file = admin
+      .storage()
+      .bucket("gs://polyglotai-76138.appspot.com")
+      .file("conversation-speech/1234-5678/user-random_id.wav");
+
+    const size = Number(file.metadata.size);
+    // const file = await bucket.get(
+    //   "conversation-speech/1234-5678/user-random_id.wav",
+    // );
+    const CHUNK_SIZE = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, size - 1);
+    const contentLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${size}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "audio/wav",
+    };
+    res.writeHead(206, headers);
+
+    const readStream = file.createReadStream({ start, end });
+    console.log(readStream);
+    readStream.pipe(res);
+  }
+
+  @Get("/get-text")
+  async getText() {
+    const bucket = admin.storage().bucket("gs://polyglotai-76138.appspot.com");
+    const file = bucket.file(
+      "conversation-speech/1234-5678/user-random_id.wav",
+    );
+    const buffer = await getRawBody(file.createReadStream());
+    const response = await AudioProcessing.fromAudioToText(buffer);
+
+    return new ApiResponse(true, response, `Transript text to: ${response}`);
   }
 }
