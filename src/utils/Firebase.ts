d0b47897043175e20a4166015c733d70441c703b
Kyrylo <kyrylocolti4@gmail.com>
2024-05-09 13:52:04 +0300

    add: create bucket instance

--- src/utils/Firebase.ts
+++ src/utils/Firebase.ts
@@ -1,9 +1,14 @@
 import admin from "firebase-admin";
+import { getStorage } from "firebase-admin/storage";
 
-const { GOOGLE_APPLICATION_CREDENTIALS } = process.env;
+const { GOOGLE_FIREBASE_APPLICATION_CREDENTIALS } = process.env;
 
 admin.initializeApp({
-  credential: admin.credential.cert(GOOGLE_APPLICATION_CREDENTIALS || ""),
+  credential: admin.credential.cert(
+    GOOGLE_FIREBASE_APPLICATION_CREDENTIALS || ""
+  ),
+  storageBucket: "polyglotai-76138.appspot.com"
 });
 
 export const db: FirebaseFirestore.Firestore = admin.firestore();
+export const bucket = getStorage().bucket();
