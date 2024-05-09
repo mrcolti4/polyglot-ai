3fa642e74b4dd1da3a66f7584bba172999c1261b
Kyrylo <kyrylocolti4@gmail.com>
2024-05-09 13:48:38 +0300

    add: create type for query parameters

--- src/types/controllerAction.ts
+++ src/types/controllerAction.ts
@@ -1,6 +1,11 @@
 import { Request } from "express";
+import { Query } from "express-serve-static-core";
 import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 
 export interface AuthRequest extends Request {
   user: DecodedIdToken;
+}
+
+export interface TypedRequest<T extends Query> extends AuthRequest {
+  query: T
 }
